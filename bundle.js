74c5a868922c6ae6db678f8facfb16ca30d7749a
CypherX <cypherx00@gmail.com>
2023-07-19 16:48:32 -0400

    Fix hatched sorting

--- bundle.js
+++ bundle.js
@@ -765,6 +765,7 @@ const pokemonRegionOverride = {
     'Hoppip (Chimecho)': GameConstants.Region.hoenn,
     'Meltan': GameConstants.Region.alola,
     'Melmetal': GameConstants.Region.alola,
+    'Flowering Celebi': GameConstants.Region.galar,
     'Magearna (Original Color)': GameConstants.Region.galar,
 
     // Event / Discord / Client
