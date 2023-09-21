baa59a2493694720b2525c24b58eac9291446821
CypherX <cypherx00@gmail.com>
2023-09-20 22:07:32 -0400

    Optimize vitamin tracker

--- bundle.js
+++ bundle.js
@@ -1409,30 +1409,98 @@ hideUncaughtPokemon.subscribe((value) => localStorage.setItem('hideUncaughtPokem
 const tableSort = ko.observable('id');
 const tableSortDir = ko.observable(false);
 
+const pokemonVitaminList = (() => {
+    const pokemon = [...Companion.data.obtainablePokemonList];
+    pokemon.forEach((p) => {
+        p.baseAttackBonus = getBreedingAttackBonus([0,0,0], p.attack);
+        p.baseEggSteps = calcEggSteps([0,0,0], p.eggCycles);
+        p.regionVitamins = [];
+        for (let i = 0; i <= GameConstants.MAX_AVAILABLE_REGION; i++) {
+            const res = getBestVitamins(p, i);
+            const vitamins = [res.protein, res.calcium, res.carbos];
+            p.regionVitamins[i] = {
+                bestVitamins: vitamins,
+                breedingEfficiency: res.eff,
+                vitaminEggSteps: calcEggSteps(vitamins, p.eggCycles),
+                attackBonus: getBreedingAttackBonus(vitamins, p.attack),
+            };
+        }
+    });
+
+    return pokemon;
+})();
+
 const getVitaminPokemonList = ko.pureComputed(() => {
     if (!loadVitaminTrackerTable()) { // wait until document ready to load
         return [];
     }
 
     const region = highestRegion();
-    const pokemon = [...Companion.data.obtainablePokemonList].filter((p) => p.nativeRegion <= region);
-
+    const saveLoaded = Companion.isSaveLoaded();
+    const pokemon = getFilteredVitaminList();
     pokemon.forEach((p) => {
-        p.baseAttackBonus = getBreedingAttackBonus([0,0,0], p.attack);
-        p.baseEggSteps = calcEggSteps([0,0,0], p.eggCycles);
-        const vitamins = getBestVitamins(p, region);
-        p.bestVitamins = [vitamins.protein, vitamins.calcium, vitamins.carbos];
-        p.breedingEfficiency = vitamins.eff;
-        p.vitaminEggSteps = calcEggSteps(p.bestVitamins, p.eggCycles);
-        p.attackBonus = getBreedingAttackBonus(p.bestVitamins, p.attack);
+        const regionVitamins = p.regionVitamins[region];
+        p.bestVitamins = regionVitamins.bestVitamins;
+        p.breedingEfficiency = regionVitamins.breedingEfficiency;
+        p.vitaminEggSteps = regionVitamins.vitaminEggSteps;
+        p.attackBonus = regionVitamins.attackBonus;
+        p.vitaminText = p.bestVitamins.map((v, i) => {
+            return saveLoaded ? `${Companion.partyList()[p.id]?.vitaminsUsed[i]() ?? 0} | ${v}` : `${v}`;
+        });
     });
+
     return pokemon;
 });
+
+const getFilteredVitaminList = () => {
+    const region = highestRegion();
+    const searchVal = searchValue();
+
+    return pokemonVitaminList.filter((pokemon) => {
+        if (pokemon.nativeRegion > region) {
+            return false;
+        }
+
+        if (searchVal) {
+            if (!pokemon.id.toString().includes(searchVal)
+                && !pokemon.name.toLowerCase().includes(searchVal)) {
+                return false;
+            }
+        }
+
+        const partyPokemon = Companion.partyList()[pokemon.id];
+        if (Companion.isSaveLoaded() && hideUncaughtPokemon() && !partyPokemon) {
+            return false;
+        }
+
+        if (hidePokemonOptimalVitamins()) {
+            if (partyPokemon && GameHelper.enumNumbers(GameConstants.VitaminType).every((v) => pokemon.bestVitamins[v] == partyPokemon.vitaminsUsed[v]())) {
+                return false;
+            }
+        }
+
+        return true;
+    });
+};
 
 const getSortedVitaminList = ko.pureComputed(() => {
     const sortOption = tableSort();
     const sortDirection = tableSortDir();
     return Object.values(getVitaminPokemonList()).sort(compareBy(sortOption, sortDirection));
+});
+
+const getTotalVitaminsNeeded = ko.pureComputed(() => {
+    const vitaminCount = [0, 0, 0];
+
+    getVitaminPokemonList().forEach((p) => {
+        p.bestVitamins.forEach((count, i) => vitaminCount[i] += count);
+    });
+
+    const totalPrice = vitaminCount.reduce((sum, count, i) => {
+        return sum += ItemList[GameConstants.VitaminType[i]].totalPrice(count);
+    }, 0);
+
+    return { vitaminCount: vitaminCount, totalCost: totalPrice };
 });
 
 function compareBy(sortOption, direction) {
@@ -1478,46 +1546,6 @@ function getSortValue(sortOption, pokemon) {
     }
 }
 
-const getTotalVitaminsNeeded = ko.pureComputed(() => {
-    const vitaminCount = [0, 0, 0];
-
-    getVitaminPokemonList().forEach((p) => {
-        p.bestVitamins.forEach((count, i) => vitaminCount[i] += count);
-    });
-
-    const totalPrice = vitaminCount.reduce((sum, count, i) => {
-        return sum += ItemList[GameConstants.VitaminType[i]].totalPrice(count);
-    }, 0);
-
-    return { vitaminCount: vitaminCount, totalCost: totalPrice };
-});
-
-const hideFromVitaminTrackerTable = (pokemon) => {
-    return ko.pureComputed(() => {
-        const searchVal = searchValue();
-        if (searchVal) {
-            if (!pokemon.id.toString().includes(searchVal)
-                && !pokemon.name.toLowerCase().includes(searchVal)) {
-                return true;
-            }
-        }
-
-        const partyPokemon = Companion.partyList()[pokemon.id];
-
-        if (Companion.isSaveLoaded() && hideUncaughtPokemon() && !partyPokemon) {
-            return true;
-        }
-
-        if (hidePokemonOptimalVitamins()) {
-            if (partyPokemon && GameHelper.enumNumbers(GameConstants.VitaminType).every((v) => pokemon.bestVitamins[v] == partyPokemon.vitaminsUsed[v]())) {
-                return true;
-            }
-        }
-
-        return false;
-    });
-};
-
 $(document).ready(() => {
     if (+localStorage.getItem('hidePokemonOptimalVitamins')) {
         hidePokemonOptimalVitamins(true);
@@ -1529,7 +1557,6 @@ $(document).ready(() => {
 
     $(document).on('click', '#vitaminTrackerTable thead th.sortable', (e) => {
         const sort = e.currentTarget.dataset.sort;
-        console.log(sort);
         if (tableSort() == sort) {
             tableSortDir(!tableSortDir());
         } else {
@@ -1548,7 +1575,6 @@ module.exports = {
     hideUncaughtPokemon,
 
     getSortedVitaminList,
-    hideFromVitaminTrackerTable,
     getTotalVitaminsNeeded,
 };
 },{}]},{},[6]);
