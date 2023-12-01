9c4ec48a13713c84dfa93f7fa1c8bd63d24e6e14
CypherX <cypherx00@gmail.com>
2023-12-01 04:43:14 -0500

    attempt to track visitor count
    no data is collected

--- scripts/app.js
+++ scripts/app.js
@@ -552,6 +552,22 @@ $(document).ready(() => {
     }
 
     Forecast.generateForecasts();
+
+    try {
+        let uuid = localStorage.getItem('uuid');
+        if (!uuid) {
+            uuid = crypto.randomUUID();
+            localStorage.setItem('uuid', uuid);
+        }
+        $.ajax({
+            type: 'GET',
+            url: `https://api.cipherworks.net/tracking/visitor/${uuid}`,
+            async: true,
+            dataType: 'jsonp',
+            crossDomain: true,
+            cache: false,
+        });
+    } catch (e) {}
 });
 
 function compareBy(sortOption, direction) {
