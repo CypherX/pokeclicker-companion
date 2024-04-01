026bc17ef352a75106b69ddcac3a8027121aaa9f
CypherX <cypherx00@gmail.com>
2024-04-01 16:51:42 -0400

    remove fuckthepolice

--- scripts/app.js
+++ scripts/app.js
@@ -492,10 +492,6 @@ const calculateTypeDamageDistribution = () => {
 const tabVisited = ko.observable({});
 const activeTab = ko.observable('#mySaveContent');
 
-if (!localStorage.getItem('fuckthepolice')) {
-    $('#prank-overlay').removeClass('d-none');
-}
-
 $(document).ready(() => {
     const container = document.getElementById('container');
     ko.applyBindings({}, container);
@@ -528,15 +524,6 @@ $(document).ready(() => {
     Forecast.generateForecasts();
 
     Util.createNotifications();
-
-    $('#fuckthepolice button').click(() => {
-        $('#prank-overlay').addClass('d-none');
-        localStorage.setItem('fuckthepolice', 1);
-    });
-
-    setTimeout(() => {
-        $('#fuckthepolice').removeClass('d-none');
-    }, 30000);
 });
 
 function compareBy(sortOption, direction) {
